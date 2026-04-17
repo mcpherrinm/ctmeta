6e755e62496d39b82d8b6d85f62ce1073550a435
Matthew McPherrin <git@mcpherrin.ca>
2026-04-17 12:14:35 -0400

    Update opgen.py for current proposal

--- opgen.py
+++ opgen.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 import argparse
-import http.client
 import json
 import sys
-import datetime
-import urllib.parse
-import io
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,23 +10,10 @@ def main():
     parser.add_argument('log', nargs='+', help='URL for log metadata')
     args = parser.parse_args()
 
-    logs = []
-    for log in args.log:
-        parsed_url = urllib.parse.urlparse(log)
-        conn = http.client.HTTPSConnection(parsed_url.netloc)
-        conn.request('GET', parsed_url.path)
-        data = json.loads(conn.getresponse().read().decode('utf-8'))
-        logs.append({
-            'friendly_name': data['friendly_name'],
-            'log_id': data['log_id'],
-            'metadata_url': log,
-        })
-
     data = {
         '$schema': 'https://www.certificate-transparency.org/schemas/operator-list-v1.json',
         'operator_name': args.name,
-        'last_updated': datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%dT%H:%M:%SZ'),
-        'logs': logs,
+        'logs': args.log,
     }
 
     json.dump(data, args.out, indent=2)
